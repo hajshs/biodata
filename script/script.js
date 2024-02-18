397ac1358c82595e7f1fba4b387540de345e342a
Joshua <jdnarag26@gmail.com>
2024-02-18 19:35:15 +0800

    added final output

---
+++ script/script.js
@@ -0,0 +1,57 @@
+function nameDisp() {
+	document.getElementById("myname").style.display = "inline";
+}
+
+function ageDisp() {
+	document.getElementById("myage").style.display = "inline";
+}
+
+function addDisp() {
+	document.getElementById("myadd").style.display = "inline";
+}
+
+function bdayDisp() {
+	document.getElementById("mybday").style.display = "inline";
+}
+
+function numDisp() {
+	document.getElementById("mynum").style.display = "inline";
+}
+
+function genderDisp() {
+	document.getElementById("mygender").style.display = "inline";
+}
+
+function eaddDisp() {
+	document.getElementById("myeadd").style.display = "inline";
+}
+
+function gsDisp() {
+	document.getElementById("mygs").style.display = "inline";
+}
+
+function hsDisp() {
+	document.getElementById("myhs").style.display = "inline";
+}
+
+function shsDisp() {
+	document.getElementById("myshs").style.display = "inline";
+}
+
+function collDisp() {
+	document.getElementById("mycoll").style.display = "inline";
+}
+
+function revAll() {
+	document.getElementById("myname").style.display = "inline";
+	document.getElementById("myage").style.display = "inline";
+	document.getElementById("myadd").style.display = "inline";
+	document.getElementById("mybday").style.display = "inline";
+	document.getElementById("mynum").style.display = "inline";
+	document.getElementById("mygender").style.display = "inline";
+	document.getElementById("myeadd").style.display = "inline";
+	document.getElementById("mygs").style.display = "inline";
+	document.getElementById("myhs").style.display = "inline";
+	document.getElementById("myshs").style.display = "inline";
+	document.getElementById("mycoll").style.display = "inline";
+}
